63f356cd6677e194c83e654018b72a9ba16df150
Zizheng Cheng <zizhengcheng@zizhengs-macbook-pro.local>
2018-04-21 16:01:01 -0700

    ziz a frigot

--- Decide For Me!/ViewController.swift
+++ Decide For Me!/ViewController.swift
@@ -7,19 +7,95 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    let URL_HEROES = "https://api.yelp.com/v3/businesses/search"
+        
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        let RestaurantFinder = RestaurantFinder(lat, long)
+        let restaurant: restaurant = RestaurantFinder.getOne()
+        restaurant.getName
+        restaurant.getImage
+        restaraunt.getLoc
+        
+        
+        
     }
-
+    
+    func getJsonFromUrl(){
+        let url = URL(string: "https://api.yelp.com/v3/businesses/search")
+        URLSession.shared.dataTask(with:url!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                if let posts = json["error"] as? [String: AnyObject] {
+                    let titor = posts["description"] as? String
+                    print("test")
+                    print(posts)
+                    print(titor)
+                }
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()
+        print("done")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
+    }
 }
 
