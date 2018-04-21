be19a4dca307a28c7b629fcc11d7aeac37720a20
Zizheng Cheng <zizhengcheng@zizhengs-macbook-pro.local>
2018-04-21 13:07:52 -0700

    Initial Commit

---
+++ Decide For Me!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Decide For Me!
+//
+//  Created by Zizheng Cheng on 4/21/18.
+//  Copyright © 2018 Lifely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
