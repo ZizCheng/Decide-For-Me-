02ed0b35dbde2d3c15ae87fec8fa740fd34e3aed
Zizheng Cheng <zizhengcheng@zizhengs-macbook-pro.local>
2018-04-22 15:18:30 -0700

    wiz's code

---
+++ Decide For Me!/SuggestionVC.swift
@@ -0,0 +1,23 @@
+//
+//  SuggestionVC.swift
+//  Decide For Me!
+//
+//  Created by Zizheng Cheng on 4/22/18.
+//  Copyright © 2018 Lifely. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SuggestionVC: UIViewController
+{
+    var lat = "0.0"
+    var long = "0.0"
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        print("we made it boys")
+    }
+}
